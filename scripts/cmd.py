207c72391fce19dcfb8767a57ed3d4ac745a1e60
Thiago Costa de Paiva <tcpaiva@cern.ch>
2019-06-06 17:27:23 +0000

    adds cmd helper script and related info in README

---
+++ scripts/cmd.py
@@ -0,0 +1,29 @@
+import socket
+import sys
+import time
+
+TCP_PORT = 2345
+
+BUFFER_SIZE = 1024
+
+def main():
+
+    target_ip_addr = sys.argv[1]
+    message = " ".join(x for x in sys.argv[2:])
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((target_ip_addr, TCP_PORT))
+    s.send(message.encode())
+
+    waiting = True
+    while (waiting):
+        data = s.recv(BUFFER_SIZE)
+        if data:
+            waiting = False
+        
+    s.close()
+
+    print(data.decode())
+
+if __name__ == "__main__":
+    main()
