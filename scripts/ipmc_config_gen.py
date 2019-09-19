dc68a24ad1337bd043618b72f56f8be472e5701a
Dan Gastler <dgastler@bu.edu>
2019-09-19 14:42:04 +0000

    Updated list of IPMCs in use

--- scripts/ipmc_config_gen.py
+++ scripts/ipmc_config_gen.py
@@ -4,15 +4,25 @@ from string import Template
 
 ip_addr_map = {}
 ip_addr_map[0] = "192.168.1.34"
+ip_addr_map[2] = "192.168.1.67"
 ip_addr_map[3] = "192.168.20.72"
 ip_addr_map[5] = "192.168.20.56"
+ip_addr_map[6] = "192.168.20.57"
 ip_addr_map[7] = "192.168.20.69"
+ip_addr_map[8] = "192.168.20.60"
+ip_addr_map[9] = "192.168.20.62"
+ip_addr_map[10] = "192.168.20.63"
 
 mac_addr_map = {}
 mac_addr_map[0] = "0A:0A:0A:0A:0A:CC"
+mac_addr_map[2] = "00:50:51:FF:00:02"
 mac_addr_map[3] = "00:50:51:FF:00:03"
 mac_addr_map[5] = "00:50:51:FF:00:05"
+mac_addr_map[6] = "00:50:51:FF:00:06"
 mac_addr_map[7] = "00:50:51:FF:00:07"
+mac_addr_map[8] = "00:50:51:FF:00:08"
+mac_addr_map[9] = "00:50:51:FF:00:09"
+mac_addr_map[10] = "00:50:51:FF:00:10"
 
 def fill_template(template_file, ipmc_id):
 
